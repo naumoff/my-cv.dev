2aeb1d9774098b1fc327dc89cd52cb2bd93e446a
naumoff <andrey.naumoff@gmail.com>
2016-08-18 16:12:42 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype HTML>
+<html lang="ru">
+<head>
+	<title>MyScriptLibrary</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/FrontEnd.css">
+</head>
+<body>
+<a href="obfuscator/index.php" target="_blank">JavaScript obfuscator and deobfuscator</a>
+<a href="robots/index.php" target="_blank">Robots.txt checker for SEO specialists</a>
+</body>
+</html>
